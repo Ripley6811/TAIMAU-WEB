20ed2c7ddd8ea10dfb54e780d0434686c02e80a7
Jay W Johnson <jwj@boun.cr>
2015-07-17 21:04:03 +0800

    Refactoring
    Added 'use strict' to a couple of functions. Will add more later.

---
+++ api/controllers/AnalysisController.js
@@ -0,0 +1,18 @@
+/**
+ * AnalysisController
+ *
+ * @description :: Server-side logic for managing analyses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+
+
+    /**
+     * `AnalysisController.index()`
+     */
+    index: function (req, res) {
+        return res.view();
+    }
+};
